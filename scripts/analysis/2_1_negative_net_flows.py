c5001b170e9cb382746f253002e3f026ae38bd80
Mattie-P <matthew.price@one.org>
2024-04-12 18:02:46 +0100

    renaming chart chart_2_1

--- scripts/analysis/2_1_negative_net_flows.py
+++ scripts/analysis/2_1_negative_net_flows.py
@@ -224,5 +224,5 @@ if __name__ == "__main__":
 
     flourish_chart_1 = flourish_1_beeswarm_pipeline(df=data)
     flourish_chart_1.to_csv(
-        Paths.output / "number_of_countries_with_nnt_2022_2025.csv", index=False
+        Paths.output / "chart_2_1.csv", index=False
     )
