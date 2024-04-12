ffe05187d3e9709e8f6d6bde20469c51920453ad
Jorge Rivera <jorge.rivera@one.org>
2024-04-12 20:17:17 +0200

    Implements regression changes
    Changes from a default of regressing by counterpart_area to counterpart_type to reduce the noise (smoother data, more linear)
    Changes the window to start in 2020 because COVID represents a structural break

--- scripts/analysis/net_flow_projections.py
+++ scripts/analysis/net_flow_projections.py
@@ -2,7 +2,11 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-from scripts.analysis.common import create_groupings, reorder_countries
+from scripts.analysis.common import (
+    create_groupings,
+    reorder_countries,
+    add_china_as_counterpart_type,
+)
 from scripts.analysis.net_flows import (
     get_all_flows,
     prep_flows,
@@ -14,17 +18,34 @@ from scripts.data.outflows import get_debt_service_data
 
 
 def calculate_linear_trend_and_predict(
-    data: pd.DataFrame, base_year: int, years_back: int = 5, years_forward: int = 3
+    data: pd.DataFrame,
+    base_year: int,
+    years_back: int = 5,
+    years_forward: int = 3,
+    creditors_grouping: str = "counterpart_area",
 ) -> pd.DataFrame:
     """Calculate linear trend over the last n years and predict for the next m years"""
+    if creditors_grouping is None:
+        creditors_grouping = ["counterpart_area"]
+
+    if isinstance(creditors_grouping, str):
+        creditors_grouping = [creditors_grouping]
+
     # Filter to last n years
+    full = data.copy()
     data = data.loc[lambda d: d.year >= base_year - years_back + 1]
 
     # group
-    group = ["year", "country", "continent", "income_level", "counterpart_area"]
+    group = ["year", "country", "continent", "income_level"] + creditors_grouping
 
     data = (
         data.groupby(group, dropna=False, observed=True)
+        .sum(numeric_only=True)
+        .reset_index()
+    )
+
+    full = (
+        full.groupby(group, dropna=False, observed=True)
         .sum(numeric_only=True)
         .reset_index()
     )
@@ -49,9 +70,9 @@ def calculate_linear_trend_and_predict(
         country,
         continent,
         income_level,
-        counterpart_area,
+        counterpart,
     ), group in regress_data.groupby(
-        ["country", "continent", "income_level", "counterpart_area"],
+        ["country", "continent", "income_level"] + creditors_grouping,
         observed=True,
         dropna=False,
     ):
@@ -70,7 +91,7 @@ def calculate_linear_trend_and_predict(
                     "country": country,
                     "continent": continent,
                     "income_level": income_level,
-                    "counterpart_area": counterpart_area,
+                    creditors_grouping[0]: counterpart,
                     "value": value,
                 }
             )
@@ -78,9 +99,11 @@ def calculate_linear_trend_and_predict(
     prediction_df = pd.DataFrame(predictions)
 
     # Limit predictions to positive values (inflows cannot be negative)
-    prediction_df = prediction_df.loc[lambda d: d.value > 0]
+    prediction_df.loc[lambda d: d.value < 0, "value"] = 0
 
-    return prediction_df.assign(indicator_type="inflow")
+    return pd.concat([full, prediction_df], ignore_index=True).assign(
+        indicator_type="inflow"
+    )
 
 
 def calculate_average_inflows(data: pd.DataFrame, years: int = 2) -> pd.DataFrame:
@@ -113,14 +136,17 @@ def calculate_average_inflows(data: pd.DataFrame, years: int = 2) -> pd.DataFram
     return data.drop(columns="value")
 
 
-def outflows_projections(constant: bool = False) -> pd.DataFrame:
+def outflows_projections(
+    constant: bool = False, creditors_grouping: str = "counterpart_area"
+) -> pd.DataFrame:
     outflows = (
         get_debt_service_data(constant=constant)
         .pipe(prep_flows)
         .assign(value=lambda d: -d.value)
         .pipe(rename_indicators, suffix="")
+        .pipe(add_china_as_counterpart_type)
         .groupby(
-            ["year", "country", "continent", "income_level", "counterpart_area"],
+            ["year", "country", "continent", "income_level", creditors_grouping],
             observed=True,
             dropna=False,
         )["value"]
@@ -128,7 +154,7 @@ def outflows_projections(constant: bool = False) -> pd.DataFrame:
         .reset_index()
     )
 
-    return outflows.query("year >= 2023")
+    return outflows
 
 
 def projected_netflows(inflows: pd.DataFrame, outflows: pd.DataFrame) -> pd.DataFrame:
@@ -163,36 +189,49 @@ def projected_netflows(inflows: pd.DataFrame, outflows: pd.DataFrame) -> pd.Data
     return data
 
 
-def projections_pipline(constant: bool = False, limit_to_2022: bool = True) -> None:
+def projections_pipline(
+    years_back: int = 3,
+    years_forward: int = 3,
+    constant: bool = False,
+    limit_to_2022: bool = True,
+    projected_only: bool = True,
+) -> None:
 
     # Get full dataset
-    data = get_all_flows(limit_to_2022=limit_to_2022, constant=constant).pipe(
-        exclude_outlier_countries
+    data = (
+        get_all_flows(limit_to_2022=limit_to_2022, constant=constant)
+        .pipe(exclude_outlier_countries)
+        .loc[lambda d: d.indicator_type == "inflow"]
+        .reset_index(drop=True)
+        .copy(deep=True)
+        .pipe(add_china_as_counterpart_type)
     )
 
     # Calculate projected inflows based on 5 year trend
-    avg_inflows = calculate_linear_trend_and_predict(
-        data=data.loc[lambda d: d.indicator_type == "inflow"]
-        .reset_index(drop=True)
-        .copy(deep=True),
+    inflows = calculate_linear_trend_and_predict(
+        data=data,
         base_year=2022,
-        years_back=5,
-        years_forward=3,
+        years_back=years_back,
+        years_forward=years_forward,
+        creditors_grouping="counterpart_type",
     )
 
     # Get outflows projections
     outflows = (
-        outflows_projections(constant=constant)
+        outflows_projections(constant=constant, creditors_grouping="counterpart_type")
         .pipe(exclude_outlier_countries)
         .assign(indicator_type="outflow")
     )
 
     # Get projected net flows
     projections = (
-        projected_netflows(inflows=avg_inflows, outflows=outflows)
+        projected_netflows(inflows=inflows, outflows=outflows)
         .drop(columns=["inflow", "outflow"])
         .rename({"net_flows": "value"}, axis=1)
     )
+
+    if projected_only:
+        projections = projections.loc[lambda d: d.year > 2022]
 
     projections_grouped = create_groupings(projections).pipe(reorder_countries)
 
